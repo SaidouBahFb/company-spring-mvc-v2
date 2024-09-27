eb711ae18c06df487d0030aa90935192728f2286
SaidouBahFb <saidoubah214@gmail.com>
2024-09-27 15:47:09 +0000

    fix: ajout des logs

--- src/main/java/com/groupeisi/companyspringmvc/service/ProductService.java
+++ src/main/java/com/groupeisi/companyspringmvc/service/ProductService.java
@@ -5,6 +5,8 @@ import com.groupeisi.companyspringmvc.dao.ProductDao;
 import com.groupeisi.companyspringmvc.dto.ProductDto;
 import com.groupeisi.companyspringmvc.entities.ProductEntity;
 import com.groupeisi.companyspringmvc.mapper.ProductMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,10 +14,14 @@ import java.util.Optional;
 
 public class ProductService implements IProductService {
 
+    private static final Logger logger = LoggerFactory.getLogger(PurchasesService.class);
+
     private IProductDao productDao = new ProductDao();
 
     @Override
     public Optional<List<ProductDto>> findAll() {
+        logger.info("PurchasesService - Tentative de liste des produits");
+
         List<ProductEntity> productEntities = productDao.list(new ProductEntity());
         List<ProductDto> productDtos = ProductMapper.toListProductDto(productEntities);
         return Optional.of(productDtos);
@@ -23,6 +29,7 @@ public class ProductService implements IProductService {
 
     @Override
     public boolean save(ProductDto productDto) {
+        logger.info("PurchasesService - Tentative d'enregistrement d'un produit : {}", productDto);
         ProductEntity productEntity = ProductMapper.toProductEntity(productDto);
         return productDao.save(productEntity);
     }
@@ -35,6 +42,7 @@ public class ProductService implements IProductService {
 
     @Override
     public boolean update(ProductDto productDto) {
+        logger.info("PurchasesService - Tentative de modification d'un produit : {}", productDto);
         ProductEntity productEntity = ProductMapper.toProductEntity(productDto);
         return productDao.update(productEntity);
     }
